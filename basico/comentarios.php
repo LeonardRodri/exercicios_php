8923e101c35cbf149c18e29a20929142d2b0067e
LeonardoRodri <65293870+LeonardRodri@users.noreply.github.com>
2021-01-08 16:24:33 -0300

    Modulo_2_Concluido

---
+++ basico/comentarios.php
@@ -0,0 +1,7 @@
+<div class="titulo">Comentarios PHP</div>
+
+<?php
+    echo "Estou no PHP";
+    //echo "Comentarios"
+    #echo "Comentarios"
+?>
--- index.php
+++ index.php
@@ -17,77 +17,24 @@
                     <div class="modulo verde">
                         <h3>Modulo 01</h3>
                         <ul>
-                            <li><a href="exercicio.php?dir=teste&file=teste">Exercícios A</a></li>
-                            <li><a href="">Exercícios B</a></li>
-                            <li><a href="">Exercícios C</a></li>
-                        </ul>
-                    </div>
-                    <div class="modulo vermelho">
-                        <h3>Modulo 02</h3>
-                        <ul>
-                            <li><a href="">Exercícios A</a></li>
-                            <li><a href="">Exercícios B</a></li>
-                            <li><a href="">Exercícios C</a></li>
-                        </ul>
-                    </div>
-                    <div class="modulo azul">
-                        <h3>Modulo 03</h3>
-                        <ul>
-                            <li><a href="">Exercícios A</a></li>
-                            <li><a href="">Exercícios B</a></li>
-                            <li><a href="">Exercícios C</a></li>
-                        </ul>
-                    </div>
-                    <div class="modulo roxo">
-                        <h3>Modulo 04</h3>
-                        <ul>
-                            <li><a href="">Exercícios A</a></li>
-                            <li><a href="">Exercícios B</a></li>
-                            <li><a href="">Exercícios C</a></li>
-                        </ul>
-                    </div>
-                    <div class="modulo laranja">
-                        <h3>Modulo 05</h3>
-                        <ul>
-                            <li><a href="">Exercícios A</a></li>
-                            <li><a href="">Exercícios B</a></li>
-                            <li><a href="">Exercícios C</a></li>
-                        </ul>
-                    </div>
-                    <div class="modulo verde-escuro">
-                        <h3>Modulo 06</h3>
-                        <ul>
-                            <li><a href="">Exercícios A</a></li>
-                            <li><a href="">Exercícios B</a></li>
-                            <li><a href="">Exercícios C</a></li>
-                        </ul>
-                    </div>
-                    <div class="modulo vermelho-escuro">
-                        <h3>Modulo 07</h3>
-                        <ul>
-                            <li><a href="">Exercícios A</a></li>
-                            <li><a href="">Exercícios B</a></li>
-                            <li><a href="">Exercícios C</a></li>
-                        </ul>
-                    </div>
-                    <div class="modulo azul-escuro">
-                        <h3>Modulo 08</h3>
-                        <ul>
-                            <li><a href="">Exercícios A</a></li>
-                            <li><a href="">Exercícios B</a></li>
-                            <li><a href="">Exercícios C</a></li>
-                        </ul>
-                    </div>
-                    <div class="modulo roxo-escuro">
-                        <h3>Modulo 09</h3>
-                        <ul>
-                            <li><a href="">Exercícios A</a></li>
-                            <li><a href="">Exercícios B</a></li>
-                            <li><a href="">Exercícios C</a></li>
+                            <li>
+                                <a href="exercicio.php?dir=basico&file=ola">Ola PHP</a>
+                            </li>
+                            <li>
+                                <a href="exercicio.php?dir=basico&file=html">Integração PHP</a>
+                            </li>
+                            <li>
+                                <a href="exercicio.php?dir=basico&file=css">Integração CSS</a>
+                            </li>
+                            <li>
+                                <a href="exercicio.php?dir=basico&file=comentarios">Comentarios PHP</a>
+                            </li>
+                            <li>
+                                <a href="exercicio.php?dir=basico&file=desafio">Desafio do modulo</a>
+                            </li>
                         </ul>
                     </div>
                 </nav>
-
             </div>
 
         </main>
